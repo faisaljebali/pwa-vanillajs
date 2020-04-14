e98cf1ae65870951bb91c7b2ff6e93d20cd766cc
Faisal Jebali <faisal.jebali@satoripop.com>
2020-04-14 22:49:59 +0100

    fix bug

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const staticDevCoffee = "recipes-app";
+const staticRecipes = "recipes-app";
 const assets = [
   "/",
   "/index.html",
@@ -14,7 +14,7 @@ const assets = [
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
+    caches.open(staticRecipes).then(cache => {
       cache.addAll(assets);
     })
   );
